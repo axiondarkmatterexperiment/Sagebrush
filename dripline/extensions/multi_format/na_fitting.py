bd684521d907392c0d90e4e6c025799fa23d9ffe
karl-destefano <kjdestef@uw.edu>
2025-02-06 14:13:15 -0800

    Create na_fitting.py

---
+++ dripline/extensions/multi_format/na_fitting.py
@@ -0,0 +1,8 @@
+import math
+import numpy as np
+import cmath
+from scipy.optimize import least_squares
+from scipy.optimize import curve_fit
+from scipy.interpolate import interp1d
+from scipy import stats
+
