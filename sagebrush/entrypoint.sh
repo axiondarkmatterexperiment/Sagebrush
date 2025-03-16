28ef48066d1d0ea9d4606a6ade95e7c9c07a04f2
Amelia <ameliabossom@gmail.com>
2025-03-16 16:38:43 -0700

    cleanup function test to remove the rabbitmq connection when container crashes

--- sagebrush/entrypoint.sh
+++ sagebrush/entrypoint.sh
@@ -9,10 +9,10 @@ trap cleanup SIGINT SIGTERM EXIT
 cleanup() {
   queue_name=`echo ${queue_name}`
   master_host_name=`echo ${master_host_name}`
-  chmod 777 /root/rabbitmqadmin
-  file="temp_${queue_name}.txt"
   user=`echo $DRIPLINE_USER`
   password=`echo $DRIPLINE_PASSWORD`
+  chmod 777 /root/rabbitmqadmin
+  file="temp_${queue_name}.txt"
   
   cmd0=`/root/rabbitmqadmin --vhost '/' -u $user -p $password -H ${master_host_name} -P "15672" list consumers channel_details.connection_name queue.name > $file`
   
