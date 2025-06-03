85bf18be9336070de615a0887e01b95f96b26c2e
Dan Zhang <dzhang95@uw.edu>
2025-06-03 11:39:13 -0700

    remove redundant dripline/extensions/__init__.py

--- dripline/extensions/__init__.py
+++
@@ -1,9 +0,0 @@
-#__path__ = __import__('pkgutil').extend_path(__path__, __name__)
-__all__ = []
-from .unstable_format import *
-from .plc import *
-from .admx_sensor_logger import *
-from .muxer_service import *
-from .multi_format import *
-from .modbus_service import *
-from .ls370 import *
