c4669062a9baba8566c6577bfc34b67203d255c6
Kieran Enzian <kenzian@cbi.admx>
2025-10-29 12:55:23 -0700

    Changing arguments to all be passed as a JSON string

--- dripline/extensions/rfsoc_data_processing.py
+++ dripline/extensions/rfsoc_data_processing.py
@@ -4,6 +4,7 @@ import yaml
 import json
 from dripline.core import Entity, calibrate, ThrowReply
 import numpy as np
+from scipy.signal import savgol_filter
 from sagebrush import rfsoc_fits as fitting
 import logging
 
@@ -21,41 +22,124 @@ class RfsocDataProcessing(Entity):
 
     #@calibrate(_all_calibrations)
 
-    def process_spectrum(self,string_spectrum,f_start,f_stop,transmission_amps,reflection_amps):
+    def process_spectrum(self,json_args):
+
+        args = json.loads(json_args)
+
+        spectrum = np.fromstring(args["string_spectrum"],dtype=np.float64,sep=',')
+        transmission_amps = np.fromstring(args["transmission_amps_str"],dtype=np.float64,sep=',')
+        reflection_amps = np.fromstring(args["reflection_amps_str"],dtype=np.float64,sep=',')
+        freqs = np.linspace(args["f_start"],args["f_stop"],len(spectrum))
+        options = json.loads(args["options_str"])
+        options["frequencies"]=freqs
+
+        output = {"Error":"False"}
+
+        #find and return indices of transmission and reflection tones. Options is a dict of arguments
+        #that specifies the method of searching for these tones and deciphering which is a 
+        # reflection/transmission tone
+        try:
+            transmission_tone_indices,reflection_tone_indices = self.find_tones_options(spectrum,options)
+        except ValueError as e:
+            return json.dumps({"Error":"True","Code":e},separators=(",",":"))
+
+        #interpolate tones to get raw power spectrum
+        tone_indices = np.sort(np.concatenate((transmission_tone_indices,reflection_tone_indices)))
+        interpolated_spectrum = self.interpolate_spectrum(spectrum,tone_indices)
+        interpolated_spectrum_fit = savgol_filter(interpolated_spectrum,window_length=50,polyorder=2)
+        output["interpolated_spec"] = np.array2string(interpolated_spectrum,separator=",",threshold=10000,max_line_width=np.inf)[1:-1]
+
+        #find tone powers with noise power subtracted
+        transmission_tone_powers = spectrum[transmission_tone_indices] - interpolated_spectrum_fit[transmission_tone_indices]
+        reflection_tone_powers = spectrum[reflection_tone_indices] - interpolated_spectrum_fit[reflection_tone_indices]
+        tone_powers = spectrum[tone_indices] - interpolated_spectrum_fit[tone_indices]
+        output["TP"] = np.array2string(transmission_tone_powers,separator=",",threshold=1000,max_line_width=np.inf)[1:-1]
+        output["RP"] = np.array2string(reflection_tone_powers,separator=",",threshold=1000,max_line_width=np.inf)[1:-1]
         
-        spectrum = np.fromstring(string_spectrum,dtype=np.float64,sep=',')
-        freqs = np.linspace(f_start,f_stop,len(spectrum))
-        tone_indices = find_tones(spectrum)
-        interpolated_spectrum = interpolate_spectrum(spectrum,tone_indices)
+        #find transmission/reflection scattering parameters
+        transmission_powers = transmission_tone_powers/(transmission_amps**2)
+        transmission_freqs = freqs[transmission_tone_indices]
+        reflection_powers = reflection_tone_powers/(reflection_amps**2)
+        reflection_freqs = freqs[reflection_tone_indices]
 
-        tone_powers = spectrum[tone_indices] - interpolated_spectrum[tone_indices]
-        tone_freqs = freqs[tone_indices]
-        
-        transmission_powers = tone_powers[::2]*transmission_amps
-        transmission_freqs = tone_freqs[::2]
-        reflection_powers = tone_powers[1::2]*reflection_amps
-        reflection_freqs = tone_freqs[1::2]
+        #try fit on the transmission/reflection scattering parameters
+        try:
+            if options["JPA_state"] == "Off":
+                transmission_fit = fitting.transmission_fit_options(transmission_powers,transmission_freqs,"cavity")
+                reflection_fit = fitting.reflection_fit_options(reflection_powers,reflection_freqs,"cavity")
+                output["TN"]=transmission_fit[0]
+                output["TF0"]=transmission_fit[1]
+                output["TQ"]=transmission_fit[2]
+                output["RN"]=reflection_fit[0]
+                output["RF0"]=reflection_fit[1]
+                output["RQ"]=reflection_fit[2]
+            elif options["JPA_state"] == "On":
+                transmission_fit = fitting.transmission_fit_options(transmission_powers,transmission_freqs,"jpa")
+                reflection_fit = fitting.reflection_fit_options(reflection_powers,reflection_freqs,"jpa")
+                output["TN"]=transmission_fit[0]
+                output["TF0"]=transmission_fit[1]
+                output["TQ"]=transmission_fit[2]
+                output["TFJPA"]=transmission_fit[3]
+                output["TQJPA"]=transmission_fit[4]
+                output["RN"]=reflection_fit[0]
+                output["RF0"]=reflection_fit[1]
+                output["RQ"]=reflection_fit[2]
+                output["RFJPA"]=reflection_fit[3]
+                output["RQJPA"]=reflection_fit[4]
+            else:
+                return json.dumps({"Error":"True","Code":"Unrecognized JPA_state"})
+            
+        except ValueError as e:
+            return json.dumps({"Error":"True","Code":e},separators=(",",":"))
 
-        transmission_fit = fitting.fit_transmission(transmission_powers,transmission_freqs)
-        reflection_fit = fitting.fit_reflection(reflection_powers,reflection_freqs)
 
-        snr = tone_powers/interpolated_spectrum[tone_indices]
-        new_amps = np.sqrt(10/snr)
-        new_transmission_amps = new_amps[::2]
-        new_reflection_amps = new_amps[1::2]
+        snr = tone_powers/interpolated_spectrum_fit[tone_indices]
+        output["SNR"]=np.array2string(snr,separator=",",threshold=1000,max_line_width=np.inf)[1:-1]
 
-        return interpolated_spectrum,transmission_fit,reflection_fit,new_transmission_amps,new_reflection_amps
+        #If JPA is off, find how amplitudes need to be adjusted. Returned is a ratio for what the new amplitudes should be compared to the old amplitudes
+        if options["JPA_state"] == "Off":
+            transmission_snr = transmission_tone_powers/interpolated_spectrum_fit[transmission_tone_indices]
+            reflection_snr = reflection_tone_powers/interpolated_spectrum_fit[reflection_tone_indices]
+            new_transmission_amps = np.min(np.array([np.sqrt(50/(transmission_snr+40)),np.ones(len(transmission_snr))]),axis=0) #the 50/(snr+40) is meant to keep the amplitudes near an snr of 10 without overcorrecting each cycle
+            new_reflection_amps = np.min(np.array([np.sqrt(50/(reflection_snr+40)),np.ones(len(reflection_snr))]),axis=0)
+            output["TA"]=np.array2string(new_transmission_amps,separator=",",threshold=1000,max_line_width=np.inf)[1:-1]
+            output["RA"]=np.array2string(new_reflection_amps,separator=",",threshold=1000,max_line_width=np.inf)[1:-1]
 
-        
+        return json.dumps(output,separators=(",",":"))
 
+    def find_tones_options(self,spec,options):
+        if options["search_method"] == "prominence":
+            tone_indices = self.find_tones_prominence(spec,options["n_tones"],options["window_len"],options["tone_ratio_low"],options["tone_ratio_high"])
+            return tone_indices[::2],tone_indices[1::2]
+        elif options["search_method"] == "frequency":
+            return self.find_tones_frequency(spec,options["frequencies"],options["transmission_frequencies"],options["reflection_frequencies"],options["window_size"])
 
+        raise ValueError("Tone search method not implemented")
+    
+    def find_tones_frequencies(self,spec,frequencies,transmission_frequencies,reflection_frequencies,window_size):
 
-    def find_max(self,string_array):
+        transmission_bins = np.floor((transmission_frequencies-frequencies[0])/(frequencies[1]-frequencies[0]),dtype=np.int32)
+        transmission_bins = transmission_bins[transmission_bins>window_size/2 and transmission_bins < len(spec)-window_size/2]
 
-        return str(np.max(np.fromstring(string_array,dtype=np.float64,sep=',')))
+        reflection_bins = np.floor((reflection_frequencies-frequencies[0])/(frequencies[1]-frequencies[0]),dtype=np.int32)
+        reflection_bins = reflection_bins[reflection_bins>window_size/2 and reflection_bins < len(spec)-window_size/2]
 
-    def find_tones(self,spec,n_tones=40,window_len=10,tone_ratio_low=0,tone_ratio_high=20):
-        
+        transmission_indices = np.zeros(len(transmission_bins))
+        for index,bin in enumerate(transmission_bins,start=0):
+            subspec = spec[bin-window_size/2:bin+window_size/2]
+            subspec_max_index = np.where(subspec == np.max(subspec))[0][0]
+            transmission_indices[index] = subspec_max_index
+
+        reflection_indices = np.zeros(len(reflection_bins))
+        for index,bin in enumerate(reflection_bins,start=0):
+            subspec = spec[bin-window_size/2:bin+window_size/2]
+            subspec_max_index = np.where(subspec == np.max(subspec))[0][0]
+            reflection_indices[index] = subspec_max_index
+
+        return transmission_indices,reflection_indices
+
+    def find_tones_prominence(self,spec,n_tones=40,window_len=10,tone_ratio_low=0,tone_ratio_high=30):
+
         spec_len = len(spec)
         tone_indices = []
         loops = 0
@@ -78,16 +162,15 @@ class RfsocDataProcessing(Entity):
                 if subspec_max >= tone_ratio*subspec_avg:
                     tone_indices.append(i+subspec_max_index)
             if len(tone_indices)==n_tones:
-                return np.array2string(np.array(tone_indices),threshold=10000,separator=',')
+                return np.array(tone_indices)
             elif len(tone_indices) > n_tones:
                 tone_ratio_low = tone_ratio
             else:
                 tone_ratio_high = tone_ratio
 
+        raise ValueError("Could not find {} tones".format(n_tones))
 
-        return None
-
-    def interpolate_spectrum(spectrum,tone_indices):
+    def interpolate_spectrum(self,spectrum,tone_indices):
         interpolated_spectrum = np.copy(spectrum)
         for index in tone_indices:
             interpolated_spectrum[index]=(interpolated_spectrum[index-1]+interpolated_spectrum[index-1])/2
--- sagebrush/rfsoc_fits.py
+++ sagebrush/rfsoc_fits.py
@@ -6,13 +6,23 @@ from scipy.optimize import curve_fit
 from scipy.interpolate import interp1d
 from scipy import stats
 
-from dripline.core import Service
 
 __all__ = []
 
+def transmission_fit_options(powers,frequencies,fit_type):
+    if fit_type == "cavity":
+        return fit_transmission_cavity(powers,frequencies)
+    elif fit_type == "jpa":
+        return fit_transmission_jpa(powers,frequencies)
+    
+def reflection_fit_options(powers,frequencies,fit_type):
+    if fit_type == "cavity":
+        return fit_reflection_cavity(powers,frequencies)
+    elif fit_type == "jpa":
+        return fit_reflection_jpa(powers,frequencies)
 
 
-def transmission_power_shape(f, norm, f0, Q):
+def transmission_power_shape_cavity(f, norm, f0, Q):
     """returns the expected power from a transmission measurement at frequency f with parameters
         f0 - resonant frequency
         Q - resonant quality
@@ -21,7 +31,18 @@ def transmission_power_shape(f, norm, f0, Q):
     delta=Q*(f-f0)/f0
     return norm*(1/(1+4*delta*delta))
 
-def reflection_power_shape(f, norm, f0, Q):
+def transmission_power_shape_jpa(f, norm, f0, Q, f_jpa, Q_jpa):
+    """returns the expected power from a transmission measurement at frequency f with parameters
+        f0 - resonant frequency
+        Q - resonant quality
+        norm - normalization of the resonance
+        noise - estimate of the noise background"""
+    delta=Q*(f-f0)/f0
+    delta_i = np.flip(delta)
+    delta_jpa=Q_jpa*(f-f_jpa)/f_jpa
+    return norm*np.max(np.array([1/(1+4*delta**2),1/(1+4*delta_i**2)]),axis=0)*(1/(1+4*delta_jpa*delta_jpa))
+
+def reflection_power_shape_cavity(f, norm, f0, Q):
     """returns the expected power from a transmission measurement at frequency f with parameters
         f0 - resonant frequency
         Q - resonant quality
@@ -30,9 +51,83 @@ def reflection_power_shape(f, norm, f0, Q):
     delta=Q*(f-f0)/f0
     return norm*(1-(1/(1+4*delta*delta)))
 
+def reflection_power_shape_jpa(f, norm, f0, Q, f_jpa, Q_jpa):
+    """returns the expected power from a transmission measurement at frequency f with parameters
+        f0 - resonant frequency
+        Q - resonant quality
+        norm - normalization of the resonance
+        noise - estimate of the noise background"""
+    delta=Q*(f-f0)/f0
+    delta_i = np.flip(delta)
+    delta_jpa=Q_jpa*(f-f_jpa)/f_jpa
+    return norm*(1-np.max(np.array([1/(1+4*delta**2),1/(1+4*delta_i**2)]),axis=0))*(1/(1+4*delta_jpa*delta_jpa))
 
 
-def fit_transmission(powers,frequencies):
+def fit_transmission_jpa(powers,frequencies):
+    """
+        Performs a least-squares fit on a transmission measurement, an array of powers and frequencies
+    """
+    if len(frequencies)!=len(powers):
+        raise ValueError("point count not right nfreqs {} npows {}".format(len(frequencies),len(powers)))
+    if len(frequencies)<16:
+        raise ValueError("not enough points to fit transmission, need 16, got {}".format(len(powers)))
+
+    f0_guess=frequencies[int(math.floor(3*len(frequencies)/4))]
+
+    norm_guess=max(powers)
+    f_jpa_guess=(frequencies[0]+frequencies[-1])/2
+    Q_min=1000
+    Q_max=100000
+    Q_guess=20000
+    Q_jpa_min = 100
+    Q_jpa_max = 20000
+    Q_jpa_guess = 5000
+    p0=[norm_guess,f0_guess,Q_guess,f_jpa_guess,Q_jpa_guess]
+    def fit_fcn(x):
+        #calculate the residuals of the fit as an array
+        nfreq=len(frequencies)
+        npriors=4
+        norm=x[0]
+        f0=x[1]
+        Q=x[2]
+        f_jpa=x[3]
+        Q_jpa=x[4]
+        resid=np.zeros(nfreq+npriors)
+        if f0<frequencies[0]:
+            resid[nfreq]=(f0-frequencies[0])/f0_guess
+            f0=frequencies[0]
+        if f0>frequencies[-1]:
+            resid[nfreq]=(frequencies[-1]-f0)/f0_guess
+            f0=frequencies[-1]
+        if Q<Q_min:
+            resid[nfreq+1]=10*nfreq*(Q-Q_min)/Q_min
+            Q=Q_min
+        if Q>Q_max:
+            resid[nfreq+1]=10*nfreq*(Q_max-Q)/Q_min
+            Q=Q_max
+        if f_jpa<frequencies[0]:
+            resid[nfreq+2]=(f_jpa-frequencies[0])/f_jpa_guess
+            f0=frequencies[0]
+        if f_jpa>frequencies[-1]:
+            resid[nfreq+2]=(frequencies[-1]-f_jpa)/f_jpa_guess
+            f0=frequencies[-1]
+        if Q_jpa>Q_jpa_max:
+            resid[nfreq+3]=10*nfreq*(Q_jpa-Q_jpa_min)/Q_jpa_min
+            Q_jpa = Q_jpa_max
+        if Q_jpa < Q_jpa_min:
+            resid[nfreq+3]=10*nfreq*(Q_jpa_max-Q_jpa)/Q_jpa_max
+            Q_jpa = Q_jpa_min
+        resid[:nfreq]=transmission_power_shape_jpa(frequencies,norm,f0,Q,f_jpa,Q_jpa) - powers
+        return resid
+    #actual fit done here
+    res=least_squares(fit_fcn,p0,xtol=1e-12) #things like df/f are super small, so set xtol extra low
+    chisq=res.cost/len(powers)
+    #contsruct the fit shape
+    fit_shape = transmission_power_shape_jpa(frequencies,res.x[0],res.x[1],res.x[2],res.x[3],res.x[4])
+    #return norm,f0,Q,norm_jpa,f_jpa,Q_jpa, chi square, fit shape
+    return [res.x[0],res.x[1],res.x[2],res.x[3],res.x[4],chisq,fit_shape]
+
+def fit_transmission_cavity_old(powers,frequencies):
     """
         Performs a least-squares fit on a transmission measurement, an array of powers and frequencies
         ASSUMPTIONS: (these go in as priors)
@@ -77,38 +172,33 @@ def fit_transmission(powers,frequencies):
             resid[nfreq+1]=10*nfreq*(Q_max-Q)/Q_min
             Q=Q_max
         for i in range(nfreq):
-            yp=transmission_power_shape(frequencies[i],norm,f0,Q)
+            yp=transmission_power_shape_cavity(frequencies[i],norm,f0,Q)
             resid[i]=(yp-powers[i])
         return resid
     #actual fit done here
     res=least_squares(fit_fcn,p0,xtol=1e-12) #things like df/f are super small, so set xtol extra low
     chisq=res.cost/len(powers)
     #contsruct the fit shape
-    fit_shape=[ transmission_power_shape(f,res.x[0],res.x[1],res.x[2]) for f in frequencies ]
+    fit_shape=[ transmission_power_shape_cavity(f,res.x[0],res.x[1],res.x[2]) for f in frequencies ]
     #return norm,f0,Q,noise, chi square, fit shape
     return [res.x[0],res.x[1],res.x[2],chisq,fit_shape]
 
-
-def fit_reflection(powers,frequencies):
+def fit_transmission_cavity(powers,frequencies):
     """
         Performs a least-squares fit on a transmission measurement, an array of powers and frequencies
-        ASSUMPTIONS: (these go in as priors)
-        - center frequency is within band
-        - band is 1-10 
-        - noise is the mean value of outer 10% of band
-        - uncertainty is standard devation of outer 10% of band
     """
     if len(frequencies)!=len(powers):
         raise ValueError("point count not right nfreqs {} npows {}".format(len(frequencies),len(powers)))
     if len(frequencies)<16:
         raise ValueError("not enough points to fit transmission, need 16, got {}".format(len(powers)))
 
-    f0_guess=frequencies[int(math.floor(len(frequencies)/2))]
-    f_band=frequencies[-1]-frequencies[0]
+    f0_guess=frequencies[int(math.floor(3*len(frequencies)/4))]
+
     norm_guess=max(powers)
-    Q_min=f0_guess/f_band
-    Q_max=20*Q_min
-    Q_guess=0.5*(Q_max+Q_min)
+    Q_min=1000
+    Q_max=100000
+    Q_guess=20000
+
     p0=[norm_guess,f0_guess,Q_guess]
     def fit_fcn(x):
         #calculate the residuals of the fit as an array
@@ -118,29 +208,135 @@ def fit_reflection(powers,frequencies):
         f0=x[1]
         Q=x[2]
         resid=np.zeros(nfreq+npriors)
-        #add priors
-        #Prior 1: frequency must be within bounds
         if f0<frequencies[0]:
             resid[nfreq]=(f0-frequencies[0])/f0_guess
             f0=frequencies[0]
         if f0>frequencies[-1]:
             resid[nfreq]=(frequencies[-1]-f0)/f0_guess
             f0=frequencies[-1]
-        #Prior 2: Q must be neither too small nor too large
         if Q<Q_min:
             resid[nfreq+1]=10*nfreq*(Q-Q_min)/Q_min
             Q=Q_min
         if Q>Q_max:
             resid[nfreq+1]=10*nfreq*(Q_max-Q)/Q_min
             Q=Q_max
-        for i in range(nfreq):
-            yp=reflection_power_shape(frequencies[i],norm,f0,Q)
-            resid[i]=(yp-powers[i])
+        resid[:nfreq]=transmission_power_shape_jpa(frequencies,norm,f0,Q) - powers
         return resid
     #actual fit done here
     res=least_squares(fit_fcn,p0,xtol=1e-12) #things like df/f are super small, so set xtol extra low
     chisq=res.cost/len(powers)
     #contsruct the fit shape
-    fit_shape=[ reflection_power_shape(f,res.x[0],res.x[1],res.x[2]) for f in frequencies ]
-    #return norm,f0,Q,noise, chi square, fit shape
+    fit_shape = transmission_power_shape_jpa(frequencies,res.x[0],res.x[1],res.x[2])
+    #return norm,f0,Q,norm_jpa,f_jpa,Q_jpa, chi square, fit shape
     return [res.x[0],res.x[1],res.x[2],chisq,fit_shape]
+
+def fit_reflection_cavity(powers,frequencies):
+    """
+        Performs a least-squares fit on a reflection measurement, an array of powers and frequencies
+    """
+    if len(frequencies)!=len(powers):
+        raise ValueError("point count not right nfreqs {} npows {}".format(len(frequencies),len(powers)))
+    if len(frequencies)<16:
+        raise ValueError("not enough points to fit reflection, need 16, got {}".format(len(powers)))
+
+    f0_guess=frequencies[int(math.floor(3*len(frequencies)/4))]
+
+    norm_guess=max(powers)
+    Q_min=1000
+    Q_max=100000
+    Q_guess=20000
+
+    p0=[norm_guess,f0_guess,Q_guess]
+    def fit_fcn(x):
+        #calculate the residuals of the fit as an array
+        nfreq=len(frequencies)
+        npriors=2
+        norm=x[0]
+        f0=x[1]
+        Q=x[2]
+        resid=np.zeros(nfreq+npriors)
+        if f0<frequencies[0]:
+            resid[nfreq]=(f0-frequencies[0])/f0_guess
+            f0=frequencies[0]
+        if f0>frequencies[-1]:
+            resid[nfreq]=(frequencies[-1]-f0)/f0_guess
+            f0=frequencies[-1]
+        if Q<Q_min:
+            resid[nfreq+1]=10*nfreq*(Q-Q_min)/Q_min
+            Q=Q_min
+        if Q>Q_max:
+            resid[nfreq+1]=10*nfreq*(Q_max-Q)/Q_min
+            Q=Q_max
+        resid[:nfreq]=reflection_power_shape_cavity(frequencies,norm,f0,Q) - powers
+        return resid
+    #actual fit done here
+    res=least_squares(fit_fcn,p0,xtol=1e-12) #things like df/f are super small, so set xtol extra low
+    chisq=res.cost/len(powers)
+    #contsruct the fit shape
+    fit_shape = reflection_power_shape_cavity(frequencies,res.x[0],res.x[1],res.x[2])
+    #return norm,f0,Q,norm_jpa,f_jpa,Q_jpa, chi square, fit shape
+    return [res.x[0],res.x[1],res.x[2],chisq,fit_shape]
+
+def fit_reflection_jpa(powers,frequencies):
+    """
+        Performs a least-squares fit on a reflection measurement, an array of powers and frequencies
+    """
+    if len(frequencies)!=len(powers):
+        raise ValueError("point count not right nfreqs {} npows {}".format(len(frequencies),len(powers)))
+    if len(frequencies)<16:
+        raise ValueError("not enough points to fit reflection, need 16, got {}".format(len(powers)))
+
+    f0_guess=frequencies[int(math.floor(3*len(frequencies)/4))]
+
+    norm_guess=max(powers)
+    f_jpa_guess=(frequencies[0]+frequencies[-1])/2
+    Q_min=1000
+    Q_max=100000
+    Q_guess=20000
+    Q_jpa_min = 100
+    Q_jpa_max = 20000
+    Q_jpa_guess = 5000
+    p0=[norm_guess,f0_guess,Q_guess,f_jpa_guess,Q_jpa_guess]
+    def fit_fcn(x):
+        #calculate the residuals of the fit as an array
+        nfreq=len(frequencies)
+        npriors=4
+        norm=x[0]
+        f0=x[1]
+        Q=x[2]
+        f_jpa=x[3]
+        Q_jpa=x[4]
+        resid=np.zeros(nfreq+npriors)
+        if f0<frequencies[0]:
+            resid[nfreq]=(f0-frequencies[0])/f0_guess
+            f0=frequencies[0]
+        if f0>frequencies[-1]:
+            resid[nfreq]=(frequencies[-1]-f0)/f0_guess
+            f0=frequencies[-1]
+        if Q<Q_min:
+            resid[nfreq+1]=10*nfreq*(Q-Q_min)/Q_min
+            Q=Q_min
+        if Q>Q_max:
+            resid[nfreq+1]=10*nfreq*(Q_max-Q)/Q_min
+            Q=Q_max
+        if f_jpa<frequencies[0]:
+            resid[nfreq+2]=(f_jpa-frequencies[0])/f_jpa_guess
+            f0=frequencies[0]
+        if f_jpa>frequencies[-1]:
+            resid[nfreq+2]=(frequencies[-1]-f_jpa)/f_jpa_guess
+            f0=frequencies[-1]
+        if Q_jpa>Q_jpa_max:
+            resid[nfreq+3]=10*nfreq*(Q_jpa-Q_jpa_min)/Q_jpa_min
+            Q_jpa = Q_jpa_max
+        if Q_jpa < Q_jpa_min:
+            resid[nfreq+3]=10*nfreq*(Q_jpa_max-Q_jpa)/Q_jpa_max
+            Q_jpa = Q_jpa_min
+        resid[:nfreq]=reflection_power_shape_jpa(frequencies,norm,f0,Q,f_jpa,Q_jpa) - powers
+        return resid
+    #actual fit done here
+    res=least_squares(fit_fcn,p0,xtol=1e-12) #things like df/f are super small, so set xtol extra low
+    chisq=res.cost/len(powers)
+    #contsruct the fit shape
+    fit_shape = reflection_power_shape_jpa(frequencies,res.x[0],res.x[1],res.x[2],res.x[3],res.x[4])
+    #return norm,f0,Q,norm_jpa,f_jpa,Q_jpa, chi square, fit shape
+    return [res.x[0],res.x[1],res.x[2],res.x[3],res.x[4],chisq,fit_shape]
